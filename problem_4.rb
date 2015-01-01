12d9c834f151a7039cd176bba8b3f8a2b2d7a267
Angus Mark <ngsmrk@gmail.com>
2015-01-01 19:41:02 +0000

    Alternative version using map

--- problem_4.rb
+++ problem_4.rb
@@ -6,9 +6,18 @@ upper_limit = 1000
 a = []
 (1...upper_limit).each do |x|
   (1...upper_limit).each do |y|
-    product = x * y
+    product = (x * y)
     a << product if palindrome?(product)
   end
 end
+
+puts a.last
+
+a = (1...upper_limit).map do |x|
+  (1...upper_limit).map do |y|
+    product = (x * y)
+    product if palindrome?(product)
+  end
+end.flatten.reject(&:nil?)
 
 puts a.last

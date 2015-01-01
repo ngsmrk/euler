6cc461bbfacaf2fb075067412fcdc6b8d35009b6
Angus Mark <ngsmrk@gmail.com>
2015-01-01 18:27:04 +0000

    Prob 3 in clojure

--- problem_3.clj
+++ problem_3.clj
@@ -1,5 +1,9 @@
-(def number 600851475143)
-
-;; use sqrt to limit range
-;; gets all prime elements between 2 and the end of the range
+(defn divisors [number]
 (filter #(= (rem number %) 0) (range 2(. Math sqrt number)))
+)
+
+(defn prime [number]
+  (= (count (divisors number)) 0)
+)
+
+(last (filter #(prime %) (divisors 600851475143)))
